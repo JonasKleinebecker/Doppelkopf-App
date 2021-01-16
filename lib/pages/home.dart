4ee64c121a8e0d5b4b43ae8d2a30ce59a4de2f72
jonaskleinebecker <jonaskleinebecker@gmail.com>
2021-01-17 00:59:30 +0100

    Initial Creation: main.dart, Home Page, startGame Page.

---
+++ lib/pages/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Doppelkopf"),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.bar_chart), onPressed: () {}),
+                IconButton(icon: Icon(Icons.person), onPressed: () {}),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 14, 0),
+                  child:
+                      IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+                )
+              ],
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/startGame");
+                    },
+                    child: Text("New Game",
+                        style: TextStyle(
+                          color: Colors.grey[900],
+                        )),
+                  ),
+                )
+              ],
+            )));
+  }
+}
---
+++ lib/pages/startGame.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class StartGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Game")
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children:<Widget> [
+            Center(child: Text("Players:",
+            style: TextStyle(fontSize: 20.0),),)
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        child: Icon(
+          Icons.add
+        )
+      ),
+    );
+  }
+}
