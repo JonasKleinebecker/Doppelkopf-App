eac4fdeb87d089c1b37372c2c1daa8e6a7538fb0
jonaskleinebecker <jonaskleinebecker@gmail.com>
2021-03-26 16:23:28 +0100

    showing games on homescreen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,19 @@
+import 'package:doppelkopf/classes/Game.dart';
+import 'package:doppelkopf/classes/GameController.dart';
+import 'package:doppelkopf/classes/Player.dart';
+import 'package:doppelkopf/classes/Round.dart';
 import 'package:doppelkopf/customWidgets/CustomCheckbox.dart';
+import 'package:doppelkopf/pages/gameOverview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,10 +37,18 @@ class Home extends StatelessWidget {
             body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Expanded(
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: GameController.getGames.length,
+                      itemBuilder: (context, index) =>
+                          generateRoundCard(index)),
+                ),
                 Center(
                   child: TextButton(
                     onPressed: () {
-                      Navigator.pushNamed(context, "/startGame");
+                      Navigator.pushNamed(context, "/startGame")
+                          .then((value) => setState(() {}));
                     },
                     child: Text("New Game",
                         style: TextStyle(
@@ -39,5 +58,125 @@ class Home extends StatelessWidget {
                 ),
               ],
             )));
+  }
+
+  generateRoundCard(int index) {
+    Game game = GameController.getGames[index];
+    List<Player> players = game.players;
+    Map<int, double> playerScores = game.playerScores;
+
+    List<Widget> widgets = [];
+
+    double playerScore;
+
+    for (int i = 0; i < players.length; i++) {
+      Player player = players[i];
+      playerScore = playerScores[player.id];
+
+      List<Widget> rowContent = [];
+
+      if (i >= (players.length - 1)) {
+        rowContent.add(
+          Container(
+              width: 45,
+              decoration: BoxDecoration(
+                border: Border.all(),
+                color: playerScore < 0 ? Colors.redAccent : Colors.lightGreen,
+                borderRadius: BorderRadius.circular(7.0),
+              ),
+              child: Center(child: Text(playerScore.toString()))),
+        );
+      } else {
+        rowContent.add(
+          Container(
+              width: 45,
+              decoration: BoxDecoration(
+                border: Border.all(),
+                color: Colors
+                    .lightGreen, //playerScore < 0 ? Colors.redAccent : Colors.lightGreen,
+                borderRadius: BorderRadius.circular(7.0),
+              ),
+              child: Center(child: Text(playerScore.toString()))),
+        );
+        rowContent.add(VerticalDivider(
+          color: Colors.grey[900],
+          thickness: 1.5,
+        ));
+      }
+
+      widgets.add(
+        Expanded(
+          child: IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: rowContent,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 6, 10, 0),
+      child: Slidable(
+        actionPane: SlidableScrollActionPane(),
+        secondaryActions: [
+          IconSlideAction(
+              icon: Icons.delete,
+              color: Colors.red,
+              onTap: () {
+                createConfirmGameDeletionDialog(context, game).then(
+                  (value) =>                 setState(() {})
+
+                );
+              })
+        ],
+        child: Card(
+          elevation: 8.0,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(color: Colors.blueGrey),
+            child: ListTile(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => GameOverview(game)));
+                },
+                contentPadding: EdgeInsets.fromLTRB(10, 13, 10, 22),
+                title: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: widgets,
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> createConfirmGameDeletionDialog(BuildContext context, Game game) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Confirm Deletion"),
+            content: Text("Do you really want to delete this game?"),
+            actions: [
+              ElevatedButton(
+                  onPressed: () {
+                    GameController.removeGame(game);
+                    Navigator.of(context)
+                        .pop(true); // return true if palyer was deleted
+                  },
+                  child: Text("Yes")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(false); // return false if player wasnt deleted
+                  },
+                  child: Text("No"))
+            ],
+          );
+        });
   }
 }
